ec40bd5149300e672ad15e93dc64e303ed0286b0
NgocBich <ctngocbich10@gmail.com>
2024-10-04 22:16:11 +0700

    commit lesson Ex06

---
+++ Ex06/app/src/main/java/com/example/ex06/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.ex06;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtten, editCMND, editBosung;
+    CheckBox chkdocbao, chkdocsach, chkdoccode;
+    Button btnsend;
+    RadioGroup group;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtten = findViewById(R.id.edtten);
+        editCMND = findViewById(R.id.edtcmnd);
+        editBosung = findViewById(R.id.edtbosung);
+        chkdocbao = findViewById(R.id.chkdocbao);
+        chkdoccode = findViewById(R.id.chkcode);
+        chkdocsach = findViewById(R.id.chkdocsach);
+        btnsend = findViewById(R.id.btnsend);
+        btnsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                doShowInformation();
+            }
+        });
+    }
+
+    public void doShowInformation() {
+        String ten = edtten.getText().toString().trim();
+        if (ten.length() < 3) {
+            edtten.requestFocus();
+            edtten.selectAll();
+            Toast.makeText(this, "Tên phải >= 3 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String cmnd = editCMND.getText().toString().trim();
+        if (cmnd.length() != 9) {
+            editCMND.requestFocus();
+            editCMND.selectAll();
+            Toast.makeText(this, "CMND phải đúng 9 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String bang = "";
+        group = findViewById(R.id.idgruop);
+        int id = group.getCheckedRadioButtonId();
+        if (id == -1) {
+            Toast.makeText(this, "Phải chọn bằng cấp", Toast.LENGTH_LONG).show();
+            return;
+        }
+        RadioButton rad = findViewById(id);
+        bang = rad.getText().toString();
+
+        String sothich = "";
+        if (chkdocbao.isChecked())
+            sothich += chkdocbao.getText() + "\n";
+        if (chkdocsach.isChecked())
+            sothich += chkdocsach.getText() + "\n";
+        if (chkdoccode.isChecked())
+            sothich += chkdoccode.getText() + "\n";
+
+        String bosung = editBosung.getText().toString();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thông tin cá nhân");
+        builder.setPositiveButton("Đóng", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        String msg = ten + "\n" + cmnd + "\n" + bang + "\n" + sothich +
+                "—————————–\n" + "Thông tin bổ sung:\n" + bosung + "\n" + "—————————–";
+        builder.setMessage(msg);
+        builder.create().show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+        b.setTitle("Question");
+        b.setMessage("Are you sure you want to exit?");
+        b.setIcon(R.drawable.ic_launcher_background); // Đảm bảo rằng bạn đã có ảnh inform trong drawable
+        b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish(); // Hoặc bạn có thể gọi super.onBackPressed();
+            }
+        });
+        b.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        b.create().show();
+
+        // Gọi super.onBackPressed() để đảm bảo hành vi mặc định vẫn được thực hiện
+        super.onBackPressed();
+    }
+
+}

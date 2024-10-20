b4480d0b5b92e33d50afdce5340b1dfae91a193d
NgocBich <ctngocbich10@gmail.com>
2024-10-21 00:21:51 +0700

    Complete Ex12

--- Ex12/app/src/main/java/com/example/ex12/MainActivity.java
+++ Ex12/app/src/main/java/com/example/ex12/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex12;
 
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lvPhone;
+    private TextView txtTitle;
+    private String[] phones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        phones = getResources().getStringArray(R.array.phones);
+        setupViews();
+        // logics
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, phones);
+        lvPhone.setAdapter(adapter);
+        lvPhone.setOnItemClickListener((parent, view, position, id) -> {
+            txtTitle.setText("Position: " + position + " - " + phones[position]);
         });
+    }
+
+    private void setupViews() {
+        lvPhone = findViewById(R.id.lv_phones);
+        txtTitle = findViewById(R.id.txt_title);
     }
 }

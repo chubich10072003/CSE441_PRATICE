29ee011e37034ab7c342b663ca2950e851346d76
NgocBich <ctngocbich10@gmail.com>
2024-10-20 23:58:22 +0700

    Complete Ex20

--- Ex20/app/src/main/java/com/example/ex20/MainActivity.java
+++ Ex20/app/src/main/java/com/example/ex20/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.note;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Note extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    MyAsyncTask asyncTask;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_note);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            asyncTask = new MyAsyncTask(this);
+            asyncTask.execute();
         });
     }
 }
